0f5c4775a5dc20d10e2549ff68d9554ba950d055
Kohilan Mohanarajan <kohilan6@hotmail.com>
2017-01-21 03:01:53 -0500

    Reversion

--- app/views.py
+++ app/views.py
@@ -47,6 +47,9 @@ def after_login(resp):
         db.session.add(user)
         db.session.commit()
 
+        # make the user follow themself
+        db.session.add(user.follow(user))
+        db.session.commit()
     remember_me = False
 
     if ('remember_me' in session):
