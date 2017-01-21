d619e7b2965a55dee9971f3d736c3b3dc11c50a3
Chintan Shah <cgintan1998@gmail.com>
2017-01-21 14:16:32 -0500

    Add location drop-down
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,14 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField, TextAreaField
+from wtforms import StringField, BooleanField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length
+
+locations = [('Bahen Centre', 'Bahen Centre'), ('Convocation Hall', 'Convocation Hall'),
+             ('Robarts', 'Robarts'), ('Gerstein', 'Gerstein'),
+             ('Hart House', 'Hart House'), ('King’s Circle', 'King’s Circle'),
+             ('Instructional Centre', 'Instructional Centre'), ('Student Centre', 'Student Centre'),
+             ('Environmental Science Building', 'Environmental Science Building'),
+             ('Bladen Wing', 'Bladen Wing'), ('Tim Hortons', 'Tim Hortons'),
+             ('UTSC Library', 'UTSC Library'), ('Pan-Am Centre', 'Pan-Am Centre')]
 
 
 class LoginForm(Form):
@@ -11,7 +19,8 @@ class LoginForm(Form):
 class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
-    location = StringField('location', validators=[DataRequired()])
+    location = SelectField('location', choices=locations,
+                           validators=[DataRequired()])
 
     def __init__(self, original_nickname, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
